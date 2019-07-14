b2bd24c84c3475cd16aa3d17c0d327ee077d634d
Paris Buttfield-Addison <paris@secretlab.com.au>
2019-07-13 17:36:17 -0700

    Arghah

--- Projects/ML-Agents/ml-agents/UnitySDK/Assets/OSCON/Activity3-RobotWarehouse/Scripts/BeepoAgent.cs
+++ Projects/ML-Agents/ml-agents/UnitySDK/Assets/OSCON/Activity3-RobotWarehouse/Scripts/BeepoAgent.cs
@@ -49,49 +49,17 @@ public class BeepoAgent : Agent, IPushAgent
 
     void Awake()
     {
-        academy = FindObjectOfType<BeepoAcademy>(); //cache the academy
-
-        goals = area.GetComponentsInChildren<CrateDestination>();
-        blocks = area.GetComponentsInChildren<Crate>();
-
-        foreach (var goal in goals)
-        {
-            goal.SetColor(academy.FindGoalDefinition(goal.type).color);
-        }
-
-        foreach (var block in blocks)
-        {
-            block.SetColor(academy.FindGoalDefinition(block.type).color);
-        }
+        // code for awake goes here
     }
 
     public override void InitializeAgent()
     {
-        base.InitializeAgent();
-
-        foreach (var block in blocks) {
-            block.agent = this;
-        }
-
-        agentRB = GetComponent<Rigidbody>();
-
-        rayPer = GetComponent<RayPerception>();
-
-        // Get the ground's bounds
-        areaBounds = ground.GetComponent<Collider>().bounds;
-        
+        // code for init goes here
     }
 
     public override void CollectObservations()
     {
-        if (useVectorObs)
-        {
-            var rayDistance = 12f;
-            float[] rayAngles = { 0f, 45f, 90f, 135f, 180f, 110f, 70f };
-            var detectableObjects = new[] { "crate", "goal", "wall" };
-            AddVectorObs(rayPer.Perceive(rayDistance, rayAngles, detectableObjects, 0f, 0f));
-            AddVectorObs(rayPer.Perceive(rayDistance, rayAngles, detectableObjects, 1.5f, 0f));
-        }
+        // code for obs goes here
     }
 
     /// <summary>
@@ -122,33 +90,13 @@ public class BeepoAgent : Agent, IPushAgent
     /// </summary>
     public void IScoredAGoal(GameObject target, GameObject goal)
     {
-        // We use a reward of 5.
-        AddReward(5f);
-        Debug.Log("Agent delivered package!");
-
-        var allGoalsComplete = true;
-        foreach (var block in blocks) {
-            if (block.IsActive == true) {
-                allGoalsComplete = false;
-            }
-        }
-
-        if (allGoalsComplete) {
-            // By marking an agent as done AgentReset() will be called automatically.
-        
-            Done();
-        }
+        // code for goal scoring goes here
     }
-
 
     public void IHitWrongGoal(GameObject target, GameObject goal)
     {
-        // We use a reward of 5.
-        AddReward(-5f);
+        // code for getting wrong goal goes here
     }
-
-
-
 
     /// <summary>
     /// Swap ground material, wait time seconds, then swap back to the
@@ -166,39 +114,7 @@ public class BeepoAgent : Agent, IPushAgent
     /// </summary>
 	public void MoveAgent(float[] act)
     {
-
-        Vector3 dirToGo = Vector3.zero;
-        Vector3 rotateDir = Vector3.zero;
-
-        int action = Mathf.FloorToInt(act[0]);
-
-        // Goalies and Strikers have slightly different action spaces.
-        switch (action)
-        {
-            case 1:
-                dirToGo = transform.forward * 1f;
-                break;
-            case 2:
-                dirToGo = transform.forward * -1f;
-                break;
-            case 3:
-                rotateDir = transform.up * 1f;
-                break;
-            case 4:
-                rotateDir = transform.up * -1f;
-                break;
-            case 5:
-                dirToGo = transform.right * -0.75f;
-                break;
-            case 6:
-                dirToGo = transform.right * 0.75f;
-                break;
-        }
-        transform.Rotate(rotateDir, Time.fixedDeltaTime * 200f);
-
-        agentRB.AddForce(dirToGo * academy.agentRunSpeed,
-                         ForceMode.VelocityChange);
-
+        // Moving code goes here
     }
 
     /// <summary>
@@ -206,11 +122,7 @@ public class BeepoAgent : Agent, IPushAgent
     /// </summary>
 	public override void AgentAction(float[] vectorAction, string textAction)
     {
-        // Move the agent using the action.
-        MoveAgent(vectorAction);
-
-        // Penalty given each step to encourage agent to finish task quickly.
-        AddReward(-1f / agentParameters.maxStep);
+        // code goes here
     }
 
     /// <summary>
@@ -232,7 +144,6 @@ public class BeepoAgent : Agent, IPushAgent
 
             block.Reset();
         }
-        
     }
 
 
@@ -243,15 +154,6 @@ public class BeepoAgent : Agent, IPushAgent
     /// </summary>
 	public override void AgentReset()
     {
-        int rotation = Random.Range(0, 4);
-        float rotationAngle = rotation * 90f;
-        area.transform.Rotate(new Vector3(0f, rotationAngle, 0f));
-
-        ResetBlocks();
-        transform.position = GetRandomSpawnPos();
-        agentRB.velocity = Vector3.zero;
-        agentRB.angularVelocity = Vector3.zero;
-
-        
+        // agent reset code goes here        
     }
 }
